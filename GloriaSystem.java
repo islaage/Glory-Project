72370c847e91dcc5f558925222d040e1297717de
islaage <34782174+islaage@users.noreply.github.com>
2018-03-07 15:45:54 +0900

    試しに変更

---
+++ GloriaSystem.java
@@ -0,0 +1,23 @@
+package com.github.islaage.GloriaSystem;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GloriaSystem extends JavaPlugin {
+
+
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Project Glory");
+        getLogger().info("Test");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+
+}
+
